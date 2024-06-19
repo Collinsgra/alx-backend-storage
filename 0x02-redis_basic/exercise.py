693761ed57cfb1d3c1141887f2bee7d424d177d5
Collinsgra <kipkemoicollins941@gmail.com>
2024-06-19 13:02:47 -0700

    Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
 """
-Reading from Redis and recovering original type
+Incrementing values
 """
 
 import redis
 import uuid
 from typing import Union, Callable, Optional
+from functools import wraps
+
+def count_calls(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        # Increment the count for this method
+        key = method.__qualname__
+        self._redis.incr(key)
+        # Call the original method
+        return method(self, *args, **kwargs)
+    return wrapper
 
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
